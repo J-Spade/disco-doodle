6b22c2fda7761ca6e828f40d515307e2a7360d8d
J-Spade <j_spade@verizon.net>
2021-05-31 18:21:17 -0400

    fix express require

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-// import Discord from 'discord.js';
-import express from 'express.js';
+const express = require('express.js');
 
 const PORT = process.env.PORT;
-
-// const bot_client = new Discord.Client();
 const webapp = express();
-
-// bot_client.once('ready', () => {
-//     console.log('Ready!');
-// });
 
 webapp.listen(PORT, function () {
     console.log(`Webapp listening on port ${PORT}`)
