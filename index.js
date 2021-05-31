aef056d28af1ef7c1d6e4a4c4d033d754c44f276
J-Spade <j_spade@verizon.net>
2021-05-31 18:17:20 -0400

    add helloworld webapp

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-import Discord from 'discord.js';
+// import Discord from 'discord.js';
+import express from 'express.js';
 
-const bot_client = new Discord.Client();
+const PORT = process.env.PORT;
 
-bot_client.once('ready', () => {
-    console.log('Ready!');
-})
+// const bot_client = new Discord.Client();
+const webapp = express();
+
+// bot_client.once('ready', () => {
+//     console.log('Ready!');
+// });
+
+webapp.listen(PORT, function () {
+    console.log(`Webapp listening on port ${PORT}`)
+});
+webapp.get('/', function (req, res) {
+    res.send('Hello World!');
+});
